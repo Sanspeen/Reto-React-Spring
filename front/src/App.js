890ddb0f8787c24104d366a892ca2c3b46e3fc6e
Santiago Franco <ktns0930@gmail.com>
2021-09-16 15:31:48 -0500

    All of components has been imported to App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,7 @@
 import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import StoreProvider from './components/StoreProvider';
 
 const HOST_API = "http://localhost:8080/api";
 
