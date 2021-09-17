fe931972610c493d7e2642adde23ee072910f71b
Santiago Franco <ktns0930@gmail.com>
2021-09-16 20:44:06 -0500

    The backend has been organized and is connected with the DB (Everything works)

---
+++ Backend-KATA/src/main/java/co/com/sofka/demoSpring/React/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package co.com.sofka.demoSpring.React.repository;
+
+import co.com.sofka.demoSpring.React.entities.Todo;
+import org.springframework.data.repository.CrudRepository;
+/*Dentro de esta interface podemos heredar todo lo que nos brinda CrudRepository que en resumen nos pide el nombre del objeto y el
+tipo de identificador que tiene este objeto para poder aplicar todas las funcionalidades de un crud.*/
+public interface TodoRepository extends CrudRepository <Todo, Long>{
+}
